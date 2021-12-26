bc5498447078ba0e52af8dbc3563c81ac19a5d37
Pablo Vejar <pgvejar@uc.cl>
2021-12-26 05:11:35 +0100

    added procsys opt file

---
+++ kern/compile/SYSCALLS/.depend.proc_syscalls.c
@@ -0,0 +1,15 @@
+proc_syscalls.o: ../../syscall/proc_syscalls.c \
+ ../../include/proc_syscalls.h ../../include/types.h \
+ ../../include/kern/types.h includelinks/kern/machine/types.h \
+ includelinks/machine/types.h ../../include/synch.h \
+ ../../include/spinlock.h ../../include/cdefs.h \
+ includelinks/machine/spinlock.h ../../include/kern/errno.h \
+ ../../include/limits.h ../../include/kern/limits.h \
+ ../../include/current.h includelinks/machine/current.h \
+ ../../include/thread.h ../../include/array.h ../../include/lib.h \
+ opt-noasserts.h ../../include/threadlist.h includelinks/machine/thread.h \
+ ../../include/setjmp.h includelinks/kern/machine/setjmp.h \
+ ../../include/proc.h ../../include/kern/fcntl.h ../../include/uio.h \
+ ../../include/kern/iovec.h ../../include/vnode.h ../../include/vfs.h \
+ ../../include/copyinout.h ../../include/kern/seek.h \
+ ../../include/kern/stat.h
