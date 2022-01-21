c0b784f46d58c0fd39d3acee60a3a5af5454a09e
pablovejar <pgvejar@uc.cl>
2022-01-22 00:51:49 +0100

    commented offset variable

--- kern/syscall/file_syscalls.c
+++ kern/syscall/file_syscalls.c
@@ -12,12 +12,14 @@
 #include <kern/seek.h>	   // for seek constants (SEEK_SET, SEEK_CUR, ..)
 #include <kern/stat.h>	   // for getting file info via VOP_STAT (stat)
 
-int sys_open(userptr_t filename, int flags, int *retval)
+
+int 
+sys_open(userptr_t filename, int flags, int *retval)
 {
 	int fd;
 	char path[PATH_MAX + 1];
 	size_t pathlen;
-	off_t offset;
+	//off_t offset;
 	struct fhandle *open_file;
 
 	DEBUG(DB_SYSCALL,
@@ -25,26 +27,18 @@ int sys_open(userptr_t filename, int flags, int *retval)
 		  filename, flags);
 
 	// check flags are legal combination
-	if (flags & O_WRONLY)
-	{
-		if (flags & !(O_WRONLY | O_CREAT | O_EXCL | O_TRUNC | O_APPEND))
-		{
+	if (flags & O_WRONLY){
+		if (flags & !(O_WRONLY | O_CREAT | O_EXCL | O_TRUNC | O_APPEND)) {
 			DEBUG(DB_SYSFILE, "Open error: Invalid flags. flags: 0x%x.\n", flags);
 			return EINVAL;
 		}
-	}
-	else if (flags & O_RDWR)
-	{
-		if (flags & !(O_RDWR | O_CREAT | O_EXCL | O_TRUNC))
-		{
+	} else if (flags & O_RDWR) {
+		if (flags & !(O_RDWR | O_CREAT | O_EXCL | O_TRUNC)) {
 			DEBUG(DB_SYSFILE, "Open error: Invalid flags. flags: 0x%x.\n", flags);
 			return EINVAL;
 		}
-	}
-	else
-	{ // O_RDONLY
-		if (flags & !(O_RDONLY | O_CREAT | O_EXCL))
-		{
+	} else { // O_RDONLY
+		if (flags & !(O_RDONLY | O_CREAT | O_EXCL)) {
 			DEBUG(DB_SYSFILE, "Open error: Invalid flags. flags: 0x%x.\n", flags);
 			return EINVAL;
 		}
@@ -52,25 +46,21 @@ int sys_open(userptr_t filename, int flags, int *retval)
 
 	// find first available slot in file table
 	fd = -1;
-	for (int i = 3; i < OPEN_MAX; i++)
-	{
-		if (curproc->p_fdtable[i] == NULL)
-		{
+	for (int i = 3; i < OPEN_MAX; i++) {
+		if (curproc->p_fdtable[i] == NULL) {
 			fd = i;
 			break;
 		}
 	}
 	// no slot found
-	if (fd == -1)
-	{
+	if (fd == -1) {
 		DEBUG(DB_SYSFILE, "Open error: File table full.\n");
 		return EMFILE;
 	}
 
 	// copy filename from userpointer into kernel buffer
 	int err = copyinstr(filename, path, sizeof(path) - 1, &pathlen);
-	if (err)
-	{
+	if (err) {
 		DEBUG(DB_SYSFILE, "Open error: Couldn't read filename.\n");
 		return err;
 	}
@@ -78,8 +68,7 @@ int sys_open(userptr_t filename, int flags, int *retval)
 	// create fhandle struct
 	open_file = kmalloc(sizeof(struct fhandle));
 	err = create_fhandle_struct(path, flags, 0, 0, open_file);
-	if (err)
-	{
+	if (err) {
 		DEBUG(DB_SYSFILE,
 			  "Open error: couldn't open file. path: %s (could be altered!),"
 			  " fd: %d, err: %d.\n",
@@ -89,12 +78,10 @@ int sys_open(userptr_t filename, int flags, int *retval)
 	}
 
 	// set offset  to EOF if O_APPEND
-	if (flags & O_APPEND)
-	{
+	if (flags & O_APPEND) {
 		struct stat *file_stat = NULL;
 		err = VOP_STAT(open_file->vn, file_stat);
-		if (err)
-		{
+		if (err) {
 			DEBUG(DB_SYSFILE,
 				  "Open error: Couldn't compute offset. err: %d\n",
 				  err);
