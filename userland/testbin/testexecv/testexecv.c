5b6174f26e3143db5215c2e5ec6738f53c77652d
pds <julian.neubert@gmx.de>
2022-01-25 17:33:47 +0100

    Fixed testexecv

--- userland/testbin/testexecv/testexecv.c
+++ userland/testbin/testexecv/testexecv.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main (void) {
-	printf("Hello world\n");
-/*
-	char* args[] = {"foo", "bar"};
+int
+main()
+{
+	char arg1[] = "foo";
+	char arg2[] = "bar";
+	char* args[] = {arg1, arg2};
 	printf("Should print foo bar now.");
 	execv("argtest", args);
-*/
 }
